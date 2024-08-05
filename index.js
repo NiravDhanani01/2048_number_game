f8ee1c5c1e363a4c1a1ad1cca87a2de1fc2dbf10
NiravDhanani <nirav.devotee@gmail.com>
2024-08-05 12:40:24 +0530

    game over pending

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const row = 4;
 let score = 0;
 let allBox = [];
 let numArray = [];
+let a = 0;
 
-// Initialize the game grid
+// display game board
 const displayGame = () => {
   game.innerHTML = "";
   allBox = [];
@@ -26,7 +27,19 @@ const displayGame = () => {
   numberGenerator(2);
 };
 
+// generate new number
 const numberGenerator = (num) => {
+  let setNum = 2;
+
+  if (score > 200) {
+    let setTarget = Math.ceil(Math.random() * 2);
+    if (setTarget <= 1) {
+      setNum = 2;
+    } else {
+      setNum = 4;
+    }
+  }
+
   for (let i = 0; i < num; i++) {
     let x = Math.floor(Math.random() * col).toString();
     let y = Math.floor(Math.random() * row).toString();
@@ -35,15 +48,24 @@ const numberGenerator = (num) => {
 
     if (box.getAttribute("test") == "false") {
       numArray.push(id);
-      box.innerHTML = "2";
+      box.innerHTML = setNum;
       box.setAttribute("test", "true");
     } else {
-      i--; 
+      i--;
+    }
+  }
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      let id = `${i}-${j}`;
+      let value = document.getElementById(id);
+      if (value.innerText === "16") {
+        gameOver();
+      }
     }
   }
 };
 
-// Handle keydown events for movement
 document.addEventListener("keydown", function (e) {
   if (e.keyCode == 37) {
     moveLeft();
@@ -60,170 +82,165 @@ document.addEventListener("keydown", function (e) {
 });
 
 const moveLeft = () => {
-    // for (let i = 0; i < row; i++) {
-    //     let currentPosition = [];
-    //     for (let j = 0; j < col; j++) {
-    //         let id = `${i}-${j}`;
-    //         let value = document.getElementById(id).innerText;
-    //         if (value !== "") {
-    //             currentPosition.push(parseInt(value));
-    //             document.getElementById(id).innerText = "";
-    //             document.getElementById(id).setAttribute("test", "false");
-    //         }
-    //     }
+  try {
+    for (let i = 0; i < row; i++) {
+      let currentPosition = [];
+      for (let j = 0; j < col; j++) {
+        let id = `${i}-${j}`;
+        let value = document.getElementById(id).innerText;
+        if (value !== "") {
+          currentPosition.push(parseInt(value));
+          document.getElementById(id).innerText = "";
+          document.getElementById(id).setAttribute("test", "false");
+        }
+      }
 
-    //     let mergePosition = [];
-    //     while (currentPosition.length > 0) {
-    //         let value = currentPosition.shift();
-    //         if (
-    //             currentPosition.length > 0 &&
-    //             currentPosition[0] === value
-    //         ) {
-    //             mergePosition.push(value * 2);
-    //             score += value * 2;
-    //             currentPosition.shift();
-    //         } else {
-    //             mergePosition.push(value);
-    //         }
-    //     }
+      let mergePosition = [];
+      while (currentPosition.length > 0) {
+        let value = currentPosition.shift();
+        if (currentPosition.length > 0 && currentPosition[0] === value) {
+          mergePosition.push(value * 2);
+          score += value * 2;
+          currentPosition.shift();
+        } else {
+          mergePosition.push(value);
+        }
+      }
 
-    //     for (let j = 0; j < col; j++) {
-    //         let id = `${i}-${j}`;
-    //         let value = mergePosition.shift();
-    //         if (value !== undefined) {
-    //             document.getElementById(id).innerText = value;
-    //             document.getElementById(id).setAttribute("test", "true");
-    //         }
-    //     }
-    // }
-    // document.getElementById("score").innerText = `${score}`;
+      for (j = 0; j < col; j++) {
+        let id = `${i}-${j}`;
+        let value = mergePosition.shift();
+        if (value !== undefined) {
+          document.getElementById(id).innerText = value;
+          document.getElementById(id).setAttribute("test", "true");
+        }
+      }
+    }
+    console.log(a);
+
+    document.getElementById("score").innerText = `${score}`;
     numberGenerator(1);
+  } catch (err) {
+    console.log(err);
+    gameOver();
+  }
 };
 
 const moveRight = () => {
-    // for (let i = 0; i < row; i++) {
-    //     let currentPosition = [];
-    //     for (let j = col - 1; j >= 0; j--) {
-    //         let id = `${i}-${j}`;
-    //         let value = document.getElementById(id).innerText;
-    //         if (value !== "") {
-    //             currentPosition.push(parseInt(value));
-    //             document.getElementById(id).innerText = "";
-    //             document.getElementById(id).setAttribute("test", "false");
-    //         }
-    //     }
+  for (let i = 0; i < row; i++) {
+    let currentPosition = [];
+    for (let j = col - 1; j >= 0; j--) {
+      let id = `${i}-${j}`;
+      let value = document.getElementById(id).innerText;
+      if (value !== "") {
+        currentPosition.push(parseInt(value));
+        document.getElementById(id).innerText = "";
+        document.getElementById(id).setAttribute("test", "false");
+      }
+    }
 
-    //     let mergePosition = [];
-    //     while (currentPosition.length > 0) {
-    //         let value = currentPosition.shift();
-    //         if (
-    //             currentPosition.length > 0 &&
-    //             currentPosition[0] === value
-    //         ) {
-    //             mergePosition.push(value * 2);
-    //             score += value * 2;
-    //             currentPosition.shift();
-    //         } else {
-    //             mergePosition.push(value);
-    //         }
-    //     }
+    let mergePosition = [];
+    while (currentPosition.length > 0) {
+      let value = currentPosition.shift();
+      if (currentPosition.length > 0 && currentPosition[0] == value) {
+        mergePosition.push(value * 2);
+        score += value * 2;
+        currentPosition.shift();
+      } else {
+        mergePosition.push(value);
+      }
+    }
 
-    //     for (let j = col - 1; j >= 0; j--) {
-    //         let id = `${i}-${j}`;
-    //         let value = mergePosition.shift();
-    //         if (value !== undefined) {
-    //             document.getElementById(id).innerText = value;
-    //             document.getElementById(id).setAttribute("test", "true");
-    //         }
-    //     }
-    // }
-    // document.getElementById("score").innerText = `${score}`;
-    numberGenerator(1);
+    for (let j = col - 1; j >= 0; j--) {
+      let id = `${i}-${j}`;
+      let value = mergePosition.shift();
+      if (value !== undefined) {
+        document.getElementById(id).innerText = value;
+        document.getElementById(id).setAttribute("test", "true");
+      }
+    }
+  }
+
+  document.getElementById("score").innerText = `${score}`;
+  numberGenerator(1);
 };
 
 const moveUp = () => {
-    for (let j = 0; j < col; j++) {
-        let currentPosition = [];
-        for (let i = 0; i < row; i++) {
-            let id = `${i}-${j}`;
-            let value = document.getElementById(id).innerText;
-            if (value !== "") {
-                currentPosition.push(parseInt(value));
-                document.getElementById(id).innerText = "";
-                document.getElementById(id).setAttribute("test", "false");
-            }
-        }
-
-        let mergePosition = [];
-        while (currentPosition.length > 0) {
-            let value = currentPosition.shift();
-            if (
-                currentPosition.length > 0 &&
-                currentPosition[0] === value
-            ) {
-                mergePosition.push(value * 2);
-                score += value * 2;
-                currentPosition.shift();
-            } else {
-                mergePosition.push(value);
-            }
-        }
-
-        for (let i = 0; i < row; i++) {
-            let id = `${i}-${j}`;
-            let value = mergePosition.shift();
-            if (value !== undefined) {
-                document.getElementById(id).innerText = value;
-                document.getElementById(id).setAttribute("test", "true");
-            }
-        }
+  for (let j = 0; j < col; j++) {
+    let currentPosition = [];
+    for (let i = 0; i < row; i++) {
+      let id = `${i}-${j}`;
+      let value = document.getElementById(id).innerText;
+      if (value !== "") {
+        currentPosition.push(parseInt(value));
+        document.getElementById(id).innerText = "";
+        document.getElementById(id).setAttribute("test", "false");
+      }
     }
-    document.getElementById("score").innerText = `${score}`;
-    numberGenerator(1);
+
+    let mergePosition = [];
+    while (currentPosition.length > 0) {
+      let value = currentPosition.shift();
+      if (currentPosition.length > 0 && currentPosition[0] === value) {
+        mergePosition.push(value * 2);
+        score += value * 2;
+        currentPosition.shift();
+      } else {
+        mergePosition.push(value);
+      }
+    }
+
+    for (let i = 0; i < row; i++) {
+      let id = `${i}-${j}`;
+      let value = mergePosition.shift();
+      if (value !== undefined) {
+        document.getElementById(id).innerText = value;
+        document.getElementById(id).setAttribute("test", "true");
+      }
+    }
+  }
+  document.getElementById("score").innerText = `${score}`;
+  numberGenerator(1);
 };
 
 const moveDown = () => {
-    for (let j = 0; j < col; j++) {
-        let currentPosition = [];
-        for (let i = row - 1; i >= 0; i--) {
-            let id = `${i}-${j}`;
-            let value = document.getElementById(id).innerText;
-            if (value !== "") {
-                currentPosition.push(parseInt(value));
-                document.getElementById(id).innerText = "";
-                document.getElementById(id).setAttribute("test", "false");
-            }
-        }
-
-        let mergePosition = [];
-        while (currentPosition.length > 0) {
-            let value = currentPosition.shift();
-            if (
-                currentPosition.length > 0 &&
-                currentPosition[0] === value
-            ) {
-                mergePosition.push(value * 2);
-                score += value * 2;
-                currentPosition.shift();
-            } else {
-                mergePosition.push(value);
-            }
-        }
-
-        for (let i = row - 1; i >= 0; i--) {
-            let id = `${i}-${j}`;
-            let value = mergePosition.shift();
-            if (value !== undefined) {
-                document.getElementById(id).innerText = value;
-                document.getElementById(id).setAttribute("test", "true");
-            }
-        }
+  for (let j = 0; j < col; j++) {
+    let currentPosition = [];
+    for (let i = row - 1; i >= 0; i--) {
+      let id = `${i}-${j}`;
+      let value = document.getElementById(id).innerText;
+      if (value !== "") {
+        currentPosition.push(parseInt(value));
+        document.getElementById(id).innerText = "";
+        document.getElementById(id).setAttribute("test", "false");
+      }
     }
-    document.getElementById("score").innerText = `${score}`;
-    numberGenerator(1);
+
+    let mergePosition = [];
+    while (currentPosition.length > 0) {
+      let value = currentPosition.shift();
+      if (currentPosition.length > 0 && currentPosition[0] === value) {
+        mergePosition.push(value * 2);
+        score += value * 2;
+        currentPosition.shift();
+      } else {
+        mergePosition.push(value);
+      }
+    }
+
+    for (let i = row - 1; i >= 0; i--) {
+      let id = `${i}-${j}`;
+      let value = mergePosition.shift();
+      if (value !== undefined) {
+        document.getElementById(id).innerText = value;
+        document.getElementById(id).setAttribute("test", "true");
+      }
+    }
+  }
+  document.getElementById("score").innerText = `${score}`;
+  numberGenerator(1);
 };
 
-// Reset and start a new game
 const newGame = () => {
   document.getElementById("score").innerText = `0`;
   score = 0;
@@ -231,4 +248,8 @@ const newGame = () => {
   displayGame();
 };
 
+const gameOver = () => {
+  alert("game Over");
+  newGame();
+};
 displayGame();
